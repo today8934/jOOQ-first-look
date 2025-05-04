b9489905ef2d930f02de22b1c96d5beb89c4d931
Wooksang Ryu <today8934@gmail.com>
2025-05-05 05:40:24 +0900

    first commit

---
+++ src/main/java/com/example/jooqfirstlook/JOoqFirstLookApplication.java
@@ -0,0 +1,13 @@
+package com.example.jooqfirstlook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JOoqFirstLookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JOoqFirstLookApplication.class, args);
+    }
+
+}
