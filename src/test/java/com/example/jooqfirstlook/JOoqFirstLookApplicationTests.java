999e276d920e91b6ca251f885a267927b1ed26ed
Wooksang Ryu <today8934@gmail.com>
2025-05-05 06:44:24 +0900

    feature: set config

--- src/test/java/com/example/jooqfirstlook/JOoqFirstLookApplicationTests.java
+++ src/test/java/com/example/jooqfirstlook/JOoqFirstLookApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.jooqfirstlook;
 
+import org.jooq.DSLContext;
+import org.jooq.generated.tables.JActor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JOoqFirstLookApplicationTests {
 
+    @Autowired
+    DSLContext dslContext;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testJOOQ() {
+        // Example of using jOOQ to fetch data
+        var result = dslContext.selectFrom(JActor.ACTOR).limit(10).fetch();
+        System.out.println(result);
     }
 
 }
